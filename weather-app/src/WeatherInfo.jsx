7bc089fc40856c87bdae1b296a00eedb55330bf1
Fatih Karaca <fatih.karaca@gmx.net>
2023-05-16 14:30:24 +0200

    add weather icon

--- weather-app/src/WeatherInfo.jsx
+++ weather-app/src/WeatherInfo.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 function WeatherInfo({ weatherData }) {
-    //console.log(weatherData);
-     if (weatherData === null) {
-       return <p>No weather data available.</p>;
-     }
+  console.log(weatherData);
+  if (weatherData === null) {
+    return <p>No weather data available.</p>;
+  }
+  const iconCode = weatherData.weather[0].icon;
+  const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
   return (
-    <div className='weatherInfo'>
+    <div className="weatherInfo">
       <h2>{weatherData.name}</h2>
+      <img src={iconURL} alt="" />
       <p>Temperatur: {weatherData.main.temp}°C</p>
       <p>Luftfeuchtigkeit: {weatherData.main.humidity}%</p>
       <p>
